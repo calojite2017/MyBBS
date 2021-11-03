8d685a535e6613a3bf53a537ef99c495f2820333
calojite2017 <calojite2017@gmail.com>
2021-11-03 14:32:01 +0900

    投稿ページの作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,15 +6,21 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    private $posts = [
+        '2021.10.3投稿',
+        '2021.10.4投稿',
+        '2021.10.5投稿',
+    ];
+
     public function index()
     {
-        $posts = [
-            '2021.10.3投稿',
-            '2021.10.4投稿',
-            '2021.10.5投稿',
-        ];
-
         return view('index')
-            ->with(['posts' => $posts]);
+            ->with(['posts' => $this->posts]);
+    }
+
+    public function show($id)
+     {
+        return view('posts.show')
+            ->with(['post' => $this->posts[$id]]);
     }
 }
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTIPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My ブログ</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>{{ $post }}</h1>
+    </div>
+</body>
+</html>
