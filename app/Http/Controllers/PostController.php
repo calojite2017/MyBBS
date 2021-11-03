407df3cddebd17624e9d1e023163953e5d7a15cd
calojite2017 <calojite2017@gmail.com>
2021-11-03 14:13:33 +0900

    postリンクの作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,9 @@ class PostController extends Controller
     public function index()
     {
         $posts = [
-            'Title A',
-            'Title B',
-            'Title C',
+            '2021.10.3投稿',
+            '2021.10.4投稿',
+            '2021.10.5投稿',
         ];
 
         return view('index')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,21 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>My BBS</title>
+    <title>My ブログ</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
-        <h1>My BBS</h1>
+        <h1>My ブログ</h1>
         <ul>
-            @forelse ($posts as $post)
-            <li>{{ $post }}</li>
+            @forelse ($posts as $index => $post)
+                <li>
+                    <a href="/posts/{{ $index }}">
+                      {{ $post }}
+                    </a>
+                </li>
             @empty
-            <li>No posts yet!</li>
+                <li>No posts yet!</li>
             @endforelse
         </ul>
     </div>
