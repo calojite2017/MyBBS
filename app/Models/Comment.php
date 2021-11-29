7889dc5984cc90ade21b2982545f1e91f00ff8c9
calojite2017 <calojite2017@gmail.com>
2021-11-29 13:07:14 +0900

    commentsテーブルとpostsテーブルを関数で紐付け

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -13,4 +13,11 @@ class Comment extends Model
         'post_id',
         'body',
     ];
+
+    // viewで「$comment->post」を使って関連ポストを抜き出したい。$this->belongsTo()は、現インスタンスのレコードは特定の投稿(Post::class)に紐づいており、postプロパティにcommentsテーブルのレコードを入れ込むよ、という作業。
+    public function post()
+    {
+        // モデル同士の関係を指定。特定のポストに対してコメントが複数ある。
+        return $this->belongsTo(Post::class);
+    }
 }
