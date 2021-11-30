72dc130f5ff4be6c0fbde88e086c5deace5a8118
calojite2017 <calojite2017@gmail.com>
2021-11-30 17:49:41 +0900

    体裁整え

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,12 @@
 <footer>
     <div id="Ft">
+        <img src="{{ asset('images/logo.jpg') }}">
+    </div>
+    <div class="company-info">
+        <ul>
+            <li>所在地</li>
+            <li>プライバシーポリシー</li>
+            <li>会社概要</li>
+        </ul>
     </div>
 </footer>
