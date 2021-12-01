03f3b30236733ede8b029e0527a6404c45ef9c2b
calojite2017 <calojite2017@gmail.com>
2021-12-01 22:54:42 +0900

    CSS整え

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 <footer>
     <div id="Ft">
         <img src="{{ asset('images/logo.jpg') }}">
-    </div>
-    <div class="company-info">
-        <ul>
-            <li>所在地　〒{{ $c_post->value }} <a href="javascript:;" onclick="window.open('http://maps.google.co.jp/maps?q='+encodeURI('{{ $c_address->value }}'));return false;">{{ $c_address->value }}</a></li>
-            <a href=""><li>プライバシーポリシー</li></a>
-            <a href=""><li>会社概要</li></a>
-        </ul>
+        <div class="company-info">
+            <ul>
+                <li>所在地　〒{{ $c_post->value }} <a href="javascript:;" onclick="window.open('http://maps.google.co.jp/maps?q='+encodeURI('{{ $c_address->value }}'));return false;">{{ $c_address->value }}</a></li>
+                <a href=""><li>プライバシーポリシー</li></a>
+                <a href=""><li>会社概要</li></a>
+            </ul>
+        </div>
     </div>
 </footer>
